ff096feef8e9d0a4f43567b523ec7973e47c683e
kamal kishore <kamal.kishore981@gmail.com>
2024-09-25 18:18:59 -0400

    update getUserRepo function

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,7 +1,14 @@
 // create function to get user repos
-
-var getUserRepos = function () {
-  fetch("https://api.github.com/users/octocat/repos");
+var getUserRepos = function (user) {
+  //format the github api url
+  var apiUrl = "https://api.github.com/users/" + user + "/repos";
+  // make a request to the url .fetch api return promise object , we then add then method to the promise object.
+  fetch(apiUrl).then(function (response) {
+    //jason method return a promise so we need to add another then()
+    response.json().then(function (data) {
+      console.log(data);
+    });
+  });
 };
-
-getUserRepos();
+console.log("outside");
+getUserRepos("kamal392");
